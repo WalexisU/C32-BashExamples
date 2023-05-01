6533ed90243d8f8cc1261a6490ae61bba53036b5
Alexis Wu <alexis.wu@hotmail.com>
2023-05-01 09:56:56 -0400

    Commandes sur echo

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
 
+# pas espaces (,) pour composer
+prenom=Emp
+echo $prenom
+
+#forcer un int
+declare -i age=33
+
+#read only
+declare -r CHEMIN=/bin
+
+echo "Nom script:" $0
+echo "Nom params:" $#
+echo "Param:" $1
