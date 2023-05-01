6fff666a89a2b2e21f39ec86e8a58f029150baff
Alexis Wu <alexis.wu@hotmail.com>
2023-05-01 10:23:36 -0400

    commandes sur conditions

--- 04-condition.sh
+++ 04-condition.sh
@@ -1,9 +1,33 @@
 #!/bin/bash
 
+# note=60
 
+#if test ...
+#if [...]
 
+read -p "note:" note 
+read -p "Entrez une lettre:" lettre
 
+if [[ $note -lt 60 ]];then 
+    echo "Desole... vous echouez"
+elif test $note -eq 60;then
+    echo "ouf!"
+else
+    echo "Beau travail"
+fi
 
+case $lettre in
+    [[:lower:]])
+        echo "La lettre est en minuscule"
+        ;;
+    *)
+        echo "Autre"
+    ;;
+esac
 
+var=a
 
+if [[ $lettre != $var ]];then
+    echo "diff de a"
+fi
 
