c94162979dd5fcd632aff42efa50081868449a08
Alexis Wu <alexis.wu@hotmail.com>
2023-05-01 09:45:17 -0400

    Ajout notes

--- 01-hello_world.sh
+++ 01-hello_world.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
 echo "Hello" $USER
+
+# exit
+
+# -n pas de saut de ligne
+echo -n "Vive"
+echo "Linux"
+
+# -e permet d'interpreter les retours de ligne et etc (\n,\t, ...)
+echo -e "1\n2\n3"
+
+
+read -p "Quel ages avez-vous?" age
+echo $age "! Vous etes jeune"
