93b2fe41b7625d3ad885235419596adbefeaa892
Alexis Wu <alexis.wu@hotmail.com>
2023-05-01 10:03:43 -0400

    commandes pour calculs

--- 03-calculs.sh
+++ 03-calculs.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
 
+echo 10+10
+echo $((10+10))
+valeur=$((10+10))
+echo $valeur
+
+read -p "nb1:" nb1
+read -p "nb2:" nb2
+
+resultat=$((nb1*nb2))
+#let resultat=$nb1*$nb2
+echo $resultat
