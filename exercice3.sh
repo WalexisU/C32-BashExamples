d89ebca76fb3d8f0b4378faadf7bf07fe3e1bcd0
Alexis Wu <alexis.wu@hotmail.com>
2023-05-01 12:20:04 -0400

    exercice_3

--- exercice3.sh
+++ exercice3.sh
@@ -1,1 +1,23 @@
 #!/bin/bash
+
+max=10;
+
+foo() {
+    for ((i = 0; i < $1; i++))
+        do   
+            echo -n " "
+        done
+}
+
+
+for((i=0; i < $max; i++))
+do
+    clear
+    foo $i
+    if [[ $i -lt 9 ]];then
+        echo "@"
+    else
+        echo "BOOM"
+    fi
+    sleep 0.1
+done
