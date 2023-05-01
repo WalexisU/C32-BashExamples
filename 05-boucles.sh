cd4e9860249d38e1b18ea4a99b5a2906dce352f0
Alexis Wu <alexis.wu@hotmail.com>
2023-05-01 10:43:56 -0400

    commandes pour boucle

--- 05-boucles.sh
+++ 05-boucles.sh
@@ -1,2 +1,21 @@
 #!/bin/bash
 
+for ((i = 0; i < 5; i++))
+do
+    echo $i
+done
+
+# `` = excecution d'une commande et mettre dans une vraiable
+liste=`ls`
+
+for fichier in $liste
+do
+    echo $fichier
+done
+
+resultat=o
+
+while [[ $resultat = o ]]
+do
+    read -p "Choix:" resultat
+done
